00d3c15d57ef553017d9f012c423ecd14dfd903b
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2014-05-16 18:08:31 +0200

    Describe colors in a programatic way

--- src/main/java/pl/poznan/put/helper/Constants.java
+++ src/main/java/pl/poznan/put/helper/Constants.java
@@ -2,7 +2,6 @@ package pl.poznan.put.helper;
 
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public final class Constants {
@@ -35,34 +34,26 @@ public final class Constants {
     public static final String UNICODE_DEGREE = "\u00B0";
     public static final String UNICODE_PI = "\u03C0";
 
-    public static final List<Color> COLORS = Arrays.asList(new Color[] {
-            Color.WHITE, new Color(173, 35, 35), new Color(42, 75, 215),
-            new Color(29, 105, 20), new Color(129, 74, 25),
-            new Color(129, 38, 192), new Color(160, 160, 160),
-            new Color(129, 197, 122), new Color(157, 175, 255),
-            new Color(41, 208, 208), new Color(255, 146, 51),
-            new Color(255, 238, 51), new Color(233, 222, 187),
-            new Color(255, 205, 243), new Color(87, 87, 87),
-            new Color(112, 219, 147), new Color(181, 166, 66),
-            new Color(95, 159, 159), new Color(184, 115, 51),
-            new Color(47, 79, 47), new Color(153, 50, 205),
-            new Color(135, 31, 120), new Color(133, 94, 66),
-            new Color(84, 84, 84), new Color(142, 35, 35),
-            new Color(245, 204, 176), new Color(35, 142, 35),
-            new Color(205, 127, 50), new Color(219, 219, 112),
-            new Color(192, 192, 192), new Color(82, 127, 118),
-            new Color(159, 159, 95), new Color(142, 35, 107),
-            new Color(47, 47, 79), new Color(235, 199, 158),
-            new Color(207, 181, 59), new Color(255, 127, 0),
-            new Color(219, 112, 219), new Color(217, 217, 243),
-            new Color(89, 89, 171), new Color(140, 23, 23),
-            new Color(35, 142, 104), new Color(107, 66, 38),
-            new Color(142, 107, 35), new Color(0, 127, 255),
-            new Color(0, 255, 127), new Color(35, 107, 142),
-            new Color(56, 176, 222), new Color(219, 147, 112),
-            new Color(173, 234, 234), new Color(92, 64, 51),
-            new Color(79, 47, 79), new Color(204, 50, 153),
-            new Color(153, 204, 50) });
+    public static final List<Color> COLORS = new ArrayList<Color>();
+
+    static {
+        float hueRed = 0.0f;
+        float hueYellow = 0.25f;
+        float hueGreen = 0.5f;
+        float hueBlue = 0.75f;
+
+        for (int i = 0; i < 5; i++) {
+            Constants.COLORS.add(Color.getHSBColor(hueRed, 0.5f, 0.5f));
+            Constants.COLORS.add(Color.getHSBColor(hueYellow, 0.5f, 0.5f));
+            Constants.COLORS.add(Color.getHSBColor(hueGreen, 0.5f, 0.5f));
+            Constants.COLORS.add(Color.getHSBColor(hueBlue, 0.5f, 0.5f));
+
+            hueRed += 0.05;
+            hueYellow += 0.05;
+            hueGreen += 0.05;
+            hueBlue += 0.05;
+        }
+    }
 
     public static List<RGB> colorsAsRGB() {
         List<RGB> result = new ArrayList<RGB>();
