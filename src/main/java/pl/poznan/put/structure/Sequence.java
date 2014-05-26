3aae77ab2b51c95b9db7918840715a5816abf1ff
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2014-05-26 22:28:12 +0200

    Add useful static factory method

--- src/main/java/pl/poznan/put/structure/Sequence.java
+++ src/main/java/pl/poznan/put/structure/Sequence.java
@@ -7,6 +7,16 @@ public class Sequence {
     private final StringBuilder builder = new StringBuilder();
     private final List<Residue> residues = new ArrayList<Residue>();
 
+    public static Sequence fromCompactFragment(CompactFragment fragment) {
+        Sequence sequence = new Sequence();
+
+        for (int i = 0; i < fragment.getSize(); i++) {
+            sequence.addResidue(fragment.getResidue(i));
+        }
+
+        return sequence;
+    }
+
     public void addResidues(List<Residue> residueList) {
         for (Residue residue : residueList) {
             addResidue(residue);
