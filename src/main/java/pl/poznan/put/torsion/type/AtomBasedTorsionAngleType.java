4185cf623da2ca190ad0f83ce71816e825be094a
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2015-04-20 09:44:59 +0200

    Add hashCode() and equals()

--- src/main/java/pl/poznan/put/torsion/type/AtomBasedTorsionAngleType.java
+++ src/main/java/pl/poznan/put/torsion/type/AtomBasedTorsionAngleType.java
@@ -9,7 +9,9 @@ public abstract class AtomBasedTorsionAngleType extends TorsionAngleType {
     private final Quadruplet<AtomName> atoms;
     private final Quadruplet<Integer> residueRule;
 
-    public AtomBasedTorsionAngleType(MoleculeType moleculeType, String displayName, Quadruplet<AtomName> atoms, Quadruplet<Integer> residueRule) {
+    public AtomBasedTorsionAngleType(MoleculeType moleculeType,
+            String displayName, Quadruplet<AtomName> atoms,
+            Quadruplet<Integer> residueRule) {
         super(moleculeType);
         this.displayName = displayName;
         this.atoms = atoms;
@@ -37,5 +39,51 @@ public abstract class AtomBasedTorsionAngleType extends TorsionAngleType {
     @Override
     public String getExportName() {
         return getClass().getSimpleName().toLowerCase();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + (atoms == null ? 0 : atoms.hashCode());
+        result = prime * result + (displayName == null ? 0 : displayName.hashCode());
+        result = prime * result + (residueRule == null ? 0 : residueRule.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        AtomBasedTorsionAngleType other = (AtomBasedTorsionAngleType) obj;
+        if (atoms == null) {
+            if (other.atoms != null) {
+                return false;
+            }
+        } else if (!atoms.equals(other.atoms)) {
+            return false;
+        }
+        if (displayName == null) {
+            if (other.displayName != null) {
+                return false;
+            }
+        } else if (!displayName.equals(other.displayName)) {
+            return false;
+        }
+        if (residueRule == null) {
+            if (other.residueRule != null) {
+                return false;
+            }
+        } else if (!residueRule.equals(other.residueRule)) {
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/pl/poznan/put/torsion/type/TorsionAngleType.java
+++ src/main/java/pl/poznan/put/torsion/type/TorsionAngleType.java
@@ -14,4 +14,30 @@ public abstract class TorsionAngleType implements DisplayableExportable {
     public MoleculeType getMoleculeType() {
         return moleculeType;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (moleculeType == null ? 0 : moleculeType.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        TorsionAngleType other = (TorsionAngleType) obj;
+        if (moleculeType != other.moleculeType) {
+            return false;
+        }
+        return true;
+    }
 }
