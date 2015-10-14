3a5715150b60fb9db80d89d863f2224ea6c9d0de
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2015-10-14 12:06:46 +0200

    Handle empty files

--- src/main/java/pl/poznan/put/structure/tertiary/StructureManager.java
+++ src/main/java/pl/poznan/put/structure/tertiary/StructureManager.java
@@ -163,6 +163,10 @@ public final class StructureManager {
     }
 
     private static boolean isGzipStream(byte[] bytes) {
+        if (bytes.length < 2) {
+            return false;
+        }
+
         int head = bytes[0] & 0xff | bytes[1] << 8 & 0xff00;
         return GZIPInputStream.GZIP_MAGIC == head;
     }
