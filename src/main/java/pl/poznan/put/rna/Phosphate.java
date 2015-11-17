cbd9befe8bd74c758337b383a833672bf6a6ddd2
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2015-11-17 19:46:27 +0100

    More additional atoms

--- src/main/java/pl/poznan/put/rna/Phosphate.java
+++ src/main/java/pl/poznan/put/rna/Phosphate.java
@@ -12,6 +12,6 @@ public class Phosphate extends NucleicAcidResidueComponent {
     }
 
     private Phosphate() {
-        super(RNAResidueComponentType.PHOSPHATE, Arrays.asList(AtomName.P, AtomName.O1P, AtomName.O2P, AtomName.O3p, AtomName.O5p), Arrays.asList(AtomName.O3P, AtomName.PA, AtomName.O1A, AtomName.O2A, AtomName.O3A, AtomName.PB, AtomName.O1B, AtomName.O2B, AtomName.O3B, AtomName.PG, AtomName.O1G, AtomName.O2G, AtomName.O3G));
+        super(RNAResidueComponentType.PHOSPHATE, Arrays.asList(AtomName.P, AtomName.O1P, AtomName.O2P, AtomName.O3p, AtomName.O5p), Arrays.asList(AtomName.O3P, AtomName.PA, AtomName.O1A, AtomName.O2A, AtomName.O3A, AtomName.PB, AtomName.O1B, AtomName.O2B, AtomName.O3B, AtomName.PC, AtomName.O1C, AtomName.O2C, AtomName.O3C, AtomName.PG, AtomName.O1G, AtomName.O2G, AtomName.O3G));
     }
 }
