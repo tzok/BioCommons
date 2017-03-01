512029269c4d6309536b8e114353cbbd0bc8399d
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2017-03-01 11:30:17 +0100

    Add test case for removal of isolated base pairs

--- src/test/java/pl/poznan/put/BpSeqTest.java
+++ src/test/java/pl/poznan/put/BpSeqTest.java
@@ -9,15 +9,15 @@ import pl.poznan.put.structure.secondary.formats.Ct;
 import pl.poznan.put.structure.secondary.formats.DotBracket;
 import pl.poznan.put.structure.secondary.formats.InvalidStructureException;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-public class TestBpSeq {
+public class BpSeqTest {
     private static final String INPUT_GOOD_1 =
             "1 A 0\n" + "2 C 3\n" + "3 G 2\n" + "4 U 0";
     private static final String INPUT_GOOD_2 =
@@ -46,78 +46,78 @@ public class TestBpSeq {
     private static final String INPUT_MAPPING_2 =
             "1 A 0\n" + "2 C 3\n" + "3 G 4\n" + "4 U 3";
 
-    private String pdb1XPO;
     private String bpseq1DDYall;
     private String bpseq1DDYnonisolated;
     private String bpseq1XPO;
+    private String pdb1XPO;
 
     @Before
     public final void loadPdbFile() throws URISyntaxException, IOException {
-        URI uri = getClass().getClassLoader().getResource(".").toURI();
-        File dir = new File(uri);
-        pdb1XPO = Helper.loadResource("1XPO.pdb");
+        bpseq1DDYall = Helper.loadResource("1DDY-A-all.bpseq");
+        bpseq1DDYnonisolated = Helper.loadResource("1DDY-A-nonisolated.bpseq");
         bpseq1XPO = Helper.loadResource("1XPO.bpseq");
+        pdb1XPO = Helper.loadResource("1XPO.pdb");
     }
 
     @Test
     public final void testGood() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_GOOD_1);
-        BpSeq.fromString(TestBpSeq.INPUT_GOOD_2);
+        BpSeq.fromString(BpSeqTest.INPUT_GOOD_1);
+        BpSeq.fromString(BpSeqTest.INPUT_GOOD_2);
     }
 
     @Test(expected = InvalidStructureException.class)
     public final void testFew() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_TOO_FEW);
+        BpSeq.fromString(BpSeqTest.INPUT_TOO_FEW);
     }
 
     @Test(expected = InvalidStructureException.class)
     public final void testMany() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_TOO_MANY);
+        BpSeq.fromString(BpSeqTest.INPUT_TOO_MANY);
     }
 
     @Test(expected = InvalidStructureException.class)
     public final void testLongSeq() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_TOO_LONG_SEQ);
+        BpSeq.fromString(BpSeqTest.INPUT_TOO_LONG_SEQ);
     }
 
     @Test(expected = InvalidStructureException.class)
     public final void testIndex1() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_INDEX_1);
+        BpSeq.fromString(BpSeqTest.INPUT_INDEX_1);
     }
 
     @Test(expected = InvalidStructureException.class)
     public final void testIndex2() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_INDEX_2);
+        BpSeq.fromString(BpSeqTest.INPUT_INDEX_2);
     }
 
     @Test(expected = InvalidStructureException.class)
     public final void testPair1() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_PAIR_1);
+        BpSeq.fromString(BpSeqTest.INPUT_PAIR_1);
     }
 
     @Test(expected = InvalidStructureException.class)
     public final void testPair2() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_PAIR_2);
+        BpSeq.fromString(BpSeqTest.INPUT_PAIR_2);
     }
 
     @Test(expected = InvalidStructureException.class)
     public final void testNumbering() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_NUMBERING);
+        BpSeq.fromString(BpSeqTest.INPUT_NUMBERING);
     }
 
     @Test(expected = InvalidStructureException.class)
     public final void testSelfPaired() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_SELF_PAIRED);
+        BpSeq.fromString(BpSeqTest.INPUT_SELF_PAIRED);
     }
 
     @Test(expected = InvalidStructureException.class)
     public final void testMapping1() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_MAPPING_1);
+        BpSeq.fromString(BpSeqTest.INPUT_MAPPING_1);
     }
 
     @Test(expected = InvalidStructureException.class)
     public final void testMapping2() throws InvalidStructureException {
-        BpSeq.fromString(TestBpSeq.INPUT_MAPPING_2);
+        BpSeq.fromString(BpSeqTest.INPUT_MAPPING_2);
     }
 
     @Test
@@ -135,5 +135,21 @@ public class TestBpSeq {
 
         BpSeq bpSeq = BpSeq.fromString(bpseq1XPO);
         Ct.fromBpSeqAndPdbModel(bpSeq, model);
+    }
+
+    @Test
+    public final void testRemovalOfIsolatedBasePairs()
+            throws InvalidStructureException {
+        BpSeq all = BpSeq.fromString(bpseq1DDYall);
+        BpSeq nonIsolated = BpSeq.fromString(bpseq1DDYnonisolated);
+        assertTrue(all.removeIsolatedPairs());
+        assertEquals(nonIsolated, all);
+    }
+
+    @Test
+    public final void testUnsucessfulRemovalOfIsolatedBasePairs()
+            throws InvalidStructureException {
+        BpSeq nonIsolated = BpSeq.fromString(bpseq1DDYnonisolated);
+        assertFalse(nonIsolated.removeIsolatedPairs());
     }
 }
