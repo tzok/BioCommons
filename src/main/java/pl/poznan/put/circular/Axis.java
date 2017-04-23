1a0dedc16275b988bfb3af745a6c3610c011d4bf
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2017-04-23 14:20:13 +0200

    Update JavaDoc

--- src/main/java/pl/poznan/put/circular/Axis.java
+++ src/main/java/pl/poznan/put/circular/Axis.java
@@ -5,7 +5,7 @@ import pl.poznan.put.circular.enums.ValueType;
 /**
  * A class of measurements where no direction can be specified (i.e. [0..180)).
  * When you analyze Axes, you should always look also at the second pole eg. 30
- * degrees -> also check 210 degrees.
+ * degrees = also check 210 degrees.
  *
  * @author tzok
  */
