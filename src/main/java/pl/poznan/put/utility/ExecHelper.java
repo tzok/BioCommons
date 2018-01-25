ac6dd2cd1c606bcde4127786515662d805dcd154
Tomasz Żok <tomasz.zok@cs.put.poznan.pl>
2018-01-25 16:06:00 +0100

    Make ExecHelper more generic and useful

--- src/main/java/pl/poznan/put/utility/ExecHelper.java
+++ src/main/java/pl/poznan/put/utility/ExecHelper.java
@@ -1,15 +1,5 @@
 package pl.poznan.put.utility;
 
-import org.apache.commons.exec.CommandLine;
-import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.ExecuteStreamHandler;
-import org.apache.commons.exec.Executor;
-import org.apache.commons.exec.PumpStreamHandler;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.output.ByteArrayOutputStream;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -17,6 +7,16 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 import java.util.UUID;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteStreamHandler;
+import org.apache.commons.exec.Executor;
+import org.apache.commons.exec.PumpStreamHandler;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.python.jline.internal.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class ExecHelper {
   public static final Logger LOGGER = LoggerFactory.getLogger(ExecHelper.class);
@@ -26,15 +26,25 @@ public final class ExecHelper {
   }
 
   public static String execute(
-      final File workingDirectory, final File command, final String... arguments)
+      final File workingDirectory, final String command, final String... arguments)
       throws IOException {
     return ExecHelper.execute(workingDirectory, Collections.emptyMap(), command, arguments);
   }
 
+  public static String execute(final String command, final String... arguments) throws IOException {
+    return ExecHelper.execute(null, Collections.emptyMap(), command, arguments);
+  }
+
   public static String execute(
-      final File workingDirectory,
+      final Map<String, String> environment, final String command, final String... arguments)
+      throws IOException {
+    return ExecHelper.execute(null, environment, command, arguments);
+  }
+
+  public static String execute(
+      @Nullable final File workingDirectory,
       final Map<String, String> environment,
-      final File command,
+      final String command,
       final String... arguments)
       throws IOException {
     // run `chmod a+x` on the command
@@ -54,15 +64,23 @@ public final class ExecHelper {
     // prepare the execution of command
     final Executor executor = new DefaultExecutor();
     executor.setStreamHandler(handler);
-    executor.setWorkingDirectory(workingDirectory);
+    if (workingDirectory != null) {
+      executor.setWorkingDirectory(workingDirectory);
+    }
 
     try {
+      if (workingDirectory != null) {
+        ExecHelper.LOGGER.info(
+            "Running {} with arguments {} in {}",
+            command,
+            Arrays.toString(arguments),
+            workingDirectory);
+      } else {
+        ExecHelper.LOGGER.info(
+            "Running {} with arguments {} in", command, Arrays.toString(arguments));
+      }
+
       // execute the command
-      ExecHelper.LOGGER.info(
-          "Running {} with arguments {} in {}",
-          command,
-          Arrays.toString(arguments),
-          workingDirectory);
       executor.execute(commandLine, environment);
 
       if (ExecHelper.LOGGER.isTraceEnabled()) {
@@ -86,12 +104,12 @@ public final class ExecHelper {
     return randomDirectory;
   }
 
-  private static void makeExecutable(final File file) {
+  private static void makeExecutable(final String path) {
     try {
       // prepare commandline
       final CommandLine chmod = new CommandLine("chmod");
       chmod.addArgument("a+x");
-      chmod.addArgument(file.getAbsolutePath());
+      chmod.addArgument(path);
 
       // prepare handler for output and error streams
       final ExecuteStreamHandler handler = new PumpStreamHandler(null, null);
