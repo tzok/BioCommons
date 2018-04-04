8be605f6c09b0f9a6e8f4696a5477f1019b99828
Tomasz Żok <tomasz.zok@cs.put.poznan.pl>
2018-04-04 12:02:05 +0200

    Add unit tests for Angle class

--- src/main/java/pl/poznan/put/circular/Angle.java
+++ src/main/java/pl/poznan/put/circular/Angle.java
@@ -1,12 +1,15 @@
 package pl.poznan.put.circular;
 
+import java.util.regex.Pattern;
+import javax.xml.bind.annotation.XmlRootElement;
 import org.apache.commons.math3.util.FastMath;
 import org.apache.commons.math3.util.MathUtils;
+import org.apache.commons.math3.util.Precision;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import pl.poznan.put.circular.enums.ValueType;
 import pl.poznan.put.circular.exception.InvalidCircularValueException;
 import pl.poznan.put.circular.exception.InvalidVectorFormatException;
-
-import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * A class of measurements where one can distinguish a direction (i.e. [0..360) degrees)
@@ -18,12 +21,14 @@ public class Angle extends Circular {
   private static final long serialVersionUID = 7888250116422842913L;
 
   private static final Angle INVALID = new Angle(Double.NaN, ValueType.RADIANS);
+  private static final Pattern DOT = Pattern.compile("[.]");
   private static final int MINUTES_IN_DAY = 24 * 60;
 
   public Angle(final double value, final ValueType valueType) {
     super(value, valueType);
   }
 
+  @Contract(pure = true)
   public static Angle invalidInstance() {
     return Angle.INVALID;
   }
@@ -38,12 +43,13 @@ public class Angle extends Circular {
    * @throws InvalidCircularValueException If the input string is parsed to a value outside the
    *     range [0..360)
    */
-  public static Angle fromHourMinuteString(final String hourMinute) {
-    final String[] split = hourMinute.split("[.]");
+  @NotNull
+  public static Angle fromHourMinuteString(@NotNull final String hourMinute) {
+    final String[] split = Angle.DOT.split(hourMinute);
 
     if (split.length != 2) {
       throw new InvalidVectorFormatException(
-          "Required format is HH.MM eg. 02.40. The input given was:" + " " + hourMinute);
+          "Required format is HH.MM eg. 02.40. The input given was: " + hourMinute);
     }
 
     try {
@@ -53,7 +59,7 @@ public class Angle extends Circular {
       return new Angle((MathUtils.TWO_PI * minutes) / Angle.MINUTES_IN_DAY, ValueType.RADIANS);
     } catch (final NumberFormatException e) {
       throw new InvalidVectorFormatException(
-          "Required format is HH.MM eg. 02.40. The input given was:" + " " + hourMinute, e);
+          "Required format is HH.MM eg. 02.40. The input given was: " + hourMinute, e);
     }
   }
 
@@ -65,23 +71,23 @@ public class Angle extends Circular {
    * @param end Ending of the range of values.
    * @return true if object is between [begin; end)
    */
-  public final boolean isBetween(final Angle begin, final Angle end) {
+  public final boolean isBetween(@NotNull final Angle begin, @NotNull final Angle end) {
     final double degrees360 = getDegrees360();
     final double begin360 = begin.getDegrees360();
     final double end360 = end.getDegrees360();
 
-    if (begin360 < end360) {
-      return (degrees360 >= begin360) && (degrees360 < end360);
-    } else {
-      return (degrees360 >= begin360) || (degrees360 < end360);
-    }
+    return (begin360 < end360)
+        ? ((degrees360 >= begin360) && (degrees360 < end360))
+        : ((degrees360 >= begin360) || (degrees360 < end360));
   }
 
-  public final Angle multiply(final double n) {
-    return new Angle((getRadians() * n) % MathUtils.TWO_PI, ValueType.RADIANS);
+  @NotNull
+  public final Angle multiply(final double v) {
+    return new Angle((getRadians() * v) % MathUtils.TWO_PI, ValueType.RADIANS);
   }
 
-  public final Angle subtract(final Angle other) {
+  @NotNull
+  public final Angle subtract(@NotNull final Angle other) {
     return new Angle(Angle.subtractByMinimum(getRadians(), other.getRadians()), ValueType.RADIANS);
   }
 
@@ -90,11 +96,37 @@ public class Angle extends Circular {
     return Math.min(d, MathUtils.TWO_PI - d);
   }
 
+  /**
+   * Calculate angles' difference using formula acos(dot(left, right)).
+   *
+   * @param left Minuend.
+   * @param right Subtrahend.
+   * @return The result of minuend - subtrahend in angular space.
+   */
   public static double subtractAsVectors(final double left, final double right) {
     double v = FastMath.sin(left) * FastMath.sin(right);
     v += FastMath.cos(left) * FastMath.cos(right);
     v = Math.min(1, v);
     v = Math.max(-1, v);
     return FastMath.acos(v);
+  }
+
+  /**
+   * Return an ordered difference between angles. It describes a rotation from one angle to another
+   * one and is therefore in range [-180; 180) degrees.
+   *
+   * @param other The other angle which value should be subtracted from this one.
+   * @return An ordered difference from first to second angle in range [-180; 180) degrees.
+   */
+  @NotNull
+  public final Angle orderedSubtract(@NotNull final Angle other) {
+    double d = getRadians() - other.getRadians();
+    while (Precision.compareTo(d, -Math.PI, 1.0e-3) < 0) {
+      d += 2.0 * Math.PI;
+    }
+    while (Precision.compareTo(d, Math.PI, 1.0e-3) > 0) {
+      d -= 2.0 * Math.PI;
+    }
+    return new Angle(d, ValueType.RADIANS);
   }
 }
