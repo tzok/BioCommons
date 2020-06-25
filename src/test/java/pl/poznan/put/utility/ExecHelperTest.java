57a18acd2e71cd972e1f121e6e25ea6d3a35dc57
Rufus <rufusikx3@gmail.com>
2020-06-25 15:07:45 +0200

    Add checkOperatingSystemCompatibility test

--- src/test/java/pl/poznan/put/utility/ExecHelperTest.java
+++ src/test/java/pl/poznan/put/utility/ExecHelperTest.java
@@ -1,7 +1,14 @@
 package pl.poznan.put.utility;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class ExecHelperTest {
-
+    @Test
+    public final void checkOperatingSystemCompatibility(){
+        Assert.assertThat(System.getProperty("os.name").toLowerCase().contains("windows"),is(false));
+    }
 }
